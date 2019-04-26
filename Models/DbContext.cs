2b132f4142c380f247787365c79255abf437375c
Unknown <julien.luccioni@protonmail.com>
2019-04-26 08:41:33 +0200

    correction

--- Models/DbContext.cs
+++ Models/DbContext.cs
@@ -36,14 +36,20 @@ namespace BaseApi.Models {
 
         public void Seed () {
             using (var db = new DBcontext ()) {
-                IEnumerable<string> blasphemes = new List<string> { "bite", "fion", "vanus", "add", "aaerr", "scout", "leto", "bad", "ont", "fia", "parse","ok" }.Select (x => x.ToUpper ());
+                IEnumerable<string> blasphemes = new List<string> { "bite", "fion", "vanus", "add", "aaerr", "scout", "leto", "bad", "ont", "fia", "parse", "ok" }.Select (x => x.ToUpper ());
                 try {
                     var lien = db.Categorizeds.Where (cs => blasphemes.Contains (cs.Category.Name.ToUpper ()));
-                    db.Categorizeds.RemoveRange (lien.ToList());
-                    db.SaveChanges ();
+                    lien.ToList ().ForEach (x => {
+                        db.Categorizeds.Remove (x);
+                        db.SaveChanges ();
+                    });
+
                     var categ = lien.Select (x => x.Category);
-                    db.RemoveRange (categ.ToList());
-                    db.SaveChanges ();
+                    categ.ToList ().ForEach (x => {
+                        db.Remove (x);
+                        db.SaveChanges ();
+                    });
+
                 } catch (Exception e) {
                     Debug.WriteLine (e.Message);
                 }
